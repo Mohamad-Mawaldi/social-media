4b643630db842df12c2160618d2eff51733044be
Mohamad <ima.mawaldi@gmail.com>
2019-03-12 22:29:59 +0100

    fixed dublecated key in list post

--- src/Component/dashboard/dashBoard.js
+++ src/Component/dashboard/dashBoard.js
@@ -8,10 +8,10 @@ export default class DashBoard extends Component {
 
     state={
         posts:[
-        {title:'First Post', id: 1, content:'First post my friends', author:'MM', status:'Good'},
-        {title:'Seconde Post', id: 2, content:'Seconde Post post my friends', author:'MX', status:'Not Good'},
-        {title:'Third Post', id: 3, content:'Third Post post my friends', author:'WA', status:'Great'},
-        {title:'Fourth Post', id: 4, content:'Fourth Post post my friends', author:'NR', status:'Bad'}]
+        {title:'First Post', id: 0, content:'First post my friends', author:'MM', status:'Good'},
+        {title:'Seconde Post', id: 1, content:'Seconde Post post my friends', author:'MX', status:'Not Good'},
+        {title:'Third Post', id: 2, content:'Third Post post my friends', author:'WA', status:'Great'},
+        {title:'Fourth Post', id: 3, content:'Fourth Post post my friends', author:'NR', status:'Bad'}]
     }
 
      addPost = (title,content) => {
